cad6aa265a9ed27a8e7e5a870f0a1074a161d1e3
BryanAdamss <BryanAdamss@foxmail.com>
2019-04-26 10:25:42 +0800

    [add] - 添加axios实例

---
+++ src/plugins/axios-instance.js
@@ -0,0 +1,23 @@
+/**
+ * @author GuangHui
+ * @description axios实例
+ */
+
+import axios from 'axios'
+import { AXIOS_DEFAULT_CONFIG } from 'Config'
+import {
+  reqResolveFn,
+  reqRejectFn,
+  respSuccFn,
+  respFailFn
+} from 'Config/interceptors/axios-interceptor'
+
+// console.log('AXIOS_DEFAULT_CONFIG', AXIOS_DEFAULT_CONFIG)
+
+const axiosInstance = axios.create(AXIOS_DEFAULT_CONFIG)
+
+axiosInstance.interceptors.request.use(reqResolveFn, reqRejectFn)
+
+axiosInstance.interceptors.response.use(respSuccFn, respFailFn)
+
+export default axiosInstance

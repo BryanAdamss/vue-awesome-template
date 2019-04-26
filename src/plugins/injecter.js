84d475c05463f6fc8f5452ed4ea35b3f5b59a4f1
BryanAdamss <BryanAdamss@foxmail.com>
2019-04-26 10:27:35 +0800

    [add] - 添加注入器

---
+++ src/plugins/injecter.js
@@ -0,0 +1,44 @@
+/**
+ * @author GuangHui
+ * @description 注入器
+ */
+import Vue from 'vue'
+
+import bus from 'Plugins/event-bus'
+import api from 'Plugins/api-builder'
+
+import { throttle, debounce } from 'Utils'
+import { GLOBAL_NAME_SPACE, BASE_URL, APP_INFO } from 'Config'
+import { Loading, Message, MessageBox } from 'element-ui'
+
+Vue.use(Loading.directive)
+
+const vueInjecter = {
+  install: (Vue, options) => {
+    Vue.prototype.$throttle = throttle
+    Vue.prototype.$debounce = debounce
+
+    Vue.prototype.$bus = bus
+    Vue.prototype.$api = api
+    Vue.prototype.$baseURL = BASE_URL
+    Vue.prototype.$globalNameSpace = GLOBAL_NAME_SPACE
+    Vue.prototype.$appInfo = APP_INFO
+
+    Vue.prototype.$loading = Loading.service
+    Vue.prototype.$msgbox = MessageBox
+    Vue.prototype.$alert = MessageBox.alert
+    Vue.prototype.$confirm = MessageBox.confirm
+    Vue.prototype.$prompt = MessageBox.prompt
+    Vue.prototype.$message = Message
+  }
+}
+
+const globalInjecter = () => {
+  window[GLOBAL_NAME_SPACE] = {
+    APP_INFO,
+    GLOBAL_NAME_SPACE,
+    $bus: bus
+  }
+}
+
+export { vueInjecter, globalInjecter }

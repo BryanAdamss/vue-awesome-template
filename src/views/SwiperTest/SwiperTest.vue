b3a0856927eb7a33742f53644e0539235c592662
BryanAdamss <BryanAdamss@foxmail.com>
2019-04-26 11:40:58 +0800

    [fix] - 调整swiper测试

--- src/views/SwiperTest/SwiperTest.vue
+++ src/views/SwiperTest/SwiperTest.vue
@@ -1,8 +1,15 @@
 <template>
-  <BaseSwiper :slides="items">
+  <BaseSwiper
+    :slides="items"
+    class="c-SwiperTest"
+  >
     <div
       slot-scope="{slide}"
-      v-text="slide.text"
+      v-text="slide.rawHtml"
+    />
+    <BaseFormulaRender
+      slot-scope="{slide}"
+      :htmlString="slide.rawHtml"
     />
   </BaseSwiper>
 </template>
@@ -13,24 +20,19 @@
  */
 
 import BaseSwiper from 'Base/BaseSwiper/BaseSwiper'
+import BaseFormulaRender from 'Base/BaseFormulaRender'
+
+import MOCK_DATA from 'Views/FormulaRenderTest/mock-data'
 
 export default {
   name: 'SwiperTest',
   components: {
-    BaseSwiper
+    BaseSwiper,
+    BaseFormulaRender
   },
   data() {
     return {
-      items: [
-        { id: 1, text: 'test1' },
-        { id: 2, text: 'test2' },
-        { id: 3, text: 'test3' },
-        { id: 4, text: 'test4' },
-        { id: 5, text: 'test5' },
-        { id: 6, text: 'test6' },
-        { id: 7, text: 'test7' },
-        { id: 8, text: 'test8' }
-      ]
+      items: MOCK_DATA
     }
   }
 }
@@ -38,5 +40,6 @@ export default {
 
 <style lang="scss" scoped>
 .c-SwiperTest {
+  @extend %noSelect;
 }
 </style>

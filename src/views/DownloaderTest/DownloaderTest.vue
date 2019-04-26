833c480bdddb5a803b16a3054130367382eed99c
BryanAdamss <BryanAdamss@foxmail.com>
2019-04-26 11:41:23 +0800

    [add] - 新增下面demo

---
+++ src/views/DownloaderTest/DownloaderTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="c-DownloaderTest">
+    <a
+      href="https://test.zhixue.com/paperdownload/download/preview/download?paperId=5b75fc64-da6d-412c-8b2d-73675d7d038c&fileFormat=docx&pageType=A4&paperType=1"
+      download="test">下载</a>
+  </div>
+</template>
+
+<script>
+/**
+ * * DownloaderTest
+ */
+import downloader from 'Common/js/downloader'
+
+export default {
+  name: 'DownloaderTest',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {
+    console.log(downloader)
+    downloader.download('https://test.zhixue.com/paperdownload/download/preview/download?paperId=5b75fc64-da6d-412c-8b2d-73675d7d038c&fileFormat=docx&pageType=A4&paperType=1')
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.c-DownloaderTest {
+}
+</style>

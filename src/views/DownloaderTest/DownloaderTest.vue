9621870ef71486bd6905626247fb84e0d0a43b7c
BryanAdamss <BryanAdamss@foxmail.com>
2019-04-26 13:58:19 +0800

    [fix] - 调整downloader路径

--- src/views/DownloaderTest/DownloaderTest.vue
+++ src/views/DownloaderTest/DownloaderTest.vue
@@ -10,7 +10,7 @@
 /**
  * * DownloaderTest
  */
-import downloader from 'Common/js/downloader'
+import Downloader from 'Plugins/downloader'
 
 export default {
   name: 'DownloaderTest',
@@ -24,7 +24,7 @@ export default {
   watch: {},
   beforeCreate() {},
   created() {
-    console.log(downloader)
+    const downloader = new Downloader()
     downloader.download('https://test.zhixue.com/paperdownload/download/preview/download?paperId=5b75fc64-da6d-412c-8b2d-73675d7d038c&fileFormat=docx&pageType=A4&paperType=1')
   },
   mounted() {},

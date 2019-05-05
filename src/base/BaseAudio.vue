e80d7a6575fa080a40780f3ca1859115c2d3911c
BryanAdamss <BryanAdamss@foxmail.com>
2019-05-05 17:49:59 +0800

    [fix] - 调整基础音频播放器逻辑

--- src/base/BaseAudio.vue
+++ src/base/BaseAudio.vue
@@ -6,12 +6,12 @@
         :class="btnClass"
         class="c-Btn"
         @click="togglePlay">
-        btn
-        {{ duration }}
-        <br>
-        {{ curTime }}
-        <br>
-        {{ progress }}
+        <template v-if="isPlaying">
+          playing
+        </template>
+        <template v-else>
+          paused
+        </template>
       </div>
 
       <template v-if="isSrcArray">
@@ -96,7 +96,16 @@
       v-if="!mini"
       class="c-BaseAudio-sub">
       <div class="c-Progress">
-        progress
+        <div
+          :style="{width:`${progress}%`}"
+          class="c-Progress-inner">
+          <BaseLoadingSpinner
+            v-if="isLoading"
+            size="1em"/>
+          <div
+            v-else
+            class="c-Progress-bar"/>
+        </div>
       </div>
     </div>
 
@@ -116,9 +125,13 @@
  * * BaseAudio
  */
 
+import BaseLoadingSpinner from 'Base/BaseLoadingSpinner'
+
 export default {
   name: 'BaseAudio',
-  components: {},
+  components: {
+    BaseLoadingSpinner
+  },
   mixins: [],
   props: {
     src: {
@@ -133,13 +146,16 @@ export default {
   data() {
     return {
       isPlaying: false,
+      isLoading: false,
+      isPaused: true,
       curTime: 0,
-      duration: 0
+      duration: 0,
+      volume: 0
     }
   },
   computed: {
     btnClass() {
-      if (this.isWaiting) {
+      if (this.isLoading) {
         return 'is-waiting'
       } else if (this.isPlaying) {
         return 'is-playing'
@@ -151,18 +167,25 @@ export default {
       return Array.isArray(this.src)
     },
     progress() {
+      if (!this.curTime || !this.duration) return 0
+
       return this.curTime / this.duration * 100
     }
   },
   watch: {},
   beforeCreate() {},
   mounted() {
+    this.volume = this.$refs.audio.volume
   },
   methods: {
     play() {
+      if (this.isLoading) return
+
       this.$refs.audio && this.$refs.audio.play()
     },
     pause() {
+      if (this.isLoading) return
+
       this.$refs.audio && this.$refs.audio.pause()
     },
     togglePlay() {
@@ -173,60 +196,124 @@ export default {
       }
     },
     // 事件处理器
-    abortHandler(e) { console.log(e.type) },
-    canplayHandler(e) { console.log(e.type) },
-    canplaythroughHandler(e) { console.log(e.type) },
+    abortHandler(e) {
+      console.log(e.type)
+      this.$emit('abort')
+    },
+    canplayHandler(e) {
+      console.log(e.type)
+      this.$emit('canplay')
+    },
+    canplaythroughHandler(e) {
+      console.log(e.type)
+      this.$emit('canplaythrough')
+    },
     durationchangeHandler(e) {
       console.log(e.type)
       this.duration = this.$refs.audio.duration
+
+      this.$emit('durationchange', this.duration)
     },
-    emptiedHandler(e) { console.log(e.type) },
+    emptiedHandler(e) {
+      console.log(e.type)
+      this.$emit('emptied')
+    },
     endedHandler(e) {
       console.log(e.type)
       this.isPlaying = false
       this.isPaused = true
-      this.isWaiting = false
+      this.isLoading = false
+
+      this.$emit('ended')
     },
-    errorHandler(e) { console.log(e.type) },
-    loadeddataHandler(e) { console.log(e.type) },
+    errorHandler(e) {
+      console.log(e.type)
+      this.$emit('error')
+    },
+    loadeddataHandler(e) {
+      console.log(e.type)
+      this.$emit('loadeddata')
+    },
     loadedmetadataHandler(e) {
       console.log(e.type)
       this.duration = this.$refs.audio.duration
+
+      this.$emit('loadedmetadata', this.duration)
     },
-    loadstartHandler(e) { console.log(e.type) },
-    mozaudioavailableHandler(e) { console.log(e.type) },
+    loadstartHandler(e) {
+      console.log(e.type)
+      this.$emit('loadstart')
+    },
+    mozaudioavailableHandler(e) {
+      console.log(e.type)
+      this.$emit('mozaudioavailable')
+    },
     pauseHandler(e) {
       console.log(e.type)
       this.isPlaying = false
       this.isPaused = true
-      this.isWaiting = false
+      this.isLoading = false
+
+      this.$emit('pause')
     },
-    playHandler(e) { console.log(e.type) },
+    playHandler(e) {
+      console.log(e.type)
+      this.$emit('play')
+    },
     playingHandler(e) {
       console.log(e.type)
       this.isPlaying = true
       this.isPaused = false
-      this.isWaiting = false
+      this.isLoading = false
+
+      this.$emit('playing')
     },
-    progressHandler(e) { console.log(e.type) },
-    ratechangeHandler(e) { console.log(e.type) },
+    progressHandler(e) {
+      console.log(e.type)
+      this.$emit('progress')
+    },
+    ratechangeHandler(e) {
+      console.log(e.type)
+      this.$emit('ratechange')
+    },
     seekedHandler(e) {
       console.log(e.type)
       this.play()
+
+      this.$emit('seeked')
     },
-    seekingHandler(e) { console.log(e.type) },
-    stalledHandler(e) { console.log(e.type) },
-    suspendHandler(e) { console.log(e.type) },
+    seekingHandler(e) {
+      console.log(e.type)
+      this.$emit('seeking')
+    },
+    stalledHandler(e) {
+      console.log(e.type)
+      this.$emit('stalled')
+    },
+    suspendHandler(e) {
+      console.log(e.type)
+      this.$emit('suspend')
+    },
     timeupdateHandler(e) {
       console.log(e.type)
       this.curTime = this.$refs.audio.currentTime
+
+      this.$emit('timeupdate', this.curTime)
     },
-    volumechangeHandler(e) { console.log(e.type) },
+    volumechangeHandler(e) {
+      console.log(e.type)
+
+      this.volume = this.$refs.audio.muted ? 0 : this.$refs.audio.volume
+
+      this.$emit('volumechange', this.volume)
+    },
     waitingHandler(e) {
       console.log(e.type)
       this.isPlaying = false
       this.isPaused = false
-      this.isWaiting = true
+      this.isLoading = true
+
+      this.$emit('waiting')
     }
   }
 }
@@ -246,9 +333,34 @@ export default {
   }
   &.is-paused {
     color: blue;
+    position: relative;
+    &::after {
+    }
   }
   &.is-waiting {
     color: pink;
+  }
+}
+
+.c-Progress {
+  height: 4px;
+  background-color: #c2c2c2;
+  padding-left: 4px;
+  padding-right: 4px;
+  &-inner {
+    height: 100%;
+    width: 0;
+    background-color: #9c9c9c;
+    position: relative;
+  }
+  &-bar {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: red;
+
+    position: absolute;
+    right: -5px;
   }
 }
 </style>

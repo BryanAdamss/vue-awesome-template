a3fe84843b6ed5596eae084bf7d912dc069bf068
BryanAdamss <BryanAdamss@foxmail.com>
2019-10-10 09:48:49 +0800

    :sparkles: 添加el组件注册器

---
+++ src/config/element-ui-list.js
@@ -0,0 +1,44 @@
+/**
+ * @author GuangHui
+ * @description element-ui list
+ */
+
+export default [
+  'Pagination',
+  'Dialog',
+  'Dropdown',
+  'DropdownMenu',
+  'DropdownItem',
+  'Input',
+  'InputNumber',
+  'Radio',
+  'RadioButton',
+  'RadioGroup',
+  'Checkbox',
+  'CheckboxButton',
+  'CheckboxGroup',
+  'Select',
+  'Option',
+  'Popover',
+  'Button',
+  'Table',
+  'TableColumn',
+  'DatePicker',
+  'Tooltip',
+  'Breadcrumb',
+  'BreadcrumbItem',
+  'Form',
+  'FormItem',
+  'Tabs',
+  'TabPane',
+  'Tree',
+  'Icon',
+  'Row',
+  'Col',
+  'Progress',
+  'Carousel',
+  'CarouselItem',
+  'Collapse',
+  'CollapseItem',
+  'Scrollbar'
+]
--- src/plugins/register.js
+++ src/plugins/register.js
@@ -7,6 +7,7 @@ import Vue from 'vue'
 import directivesMap from 'Directives'
 
 import filtersMap from 'Config/filters-map'
+import elementUIList from 'Config/element-ui-list'
 
 export function directiveRegister() {
   for (let key in directivesMap) {
@@ -19,4 +20,12 @@ export function directiveRegister() {
 
 export function filterRegister() {
   Object.entries(filtersMap).forEach(([key, value]) => Vue.filter(key, value))
+}
+
+export function elCompRegister() {
+  const str2kebab = str => str.replace(/\B([A-Z])/g, '-$1').toLowerCase()
+  elementUIList.forEach(compName => {
+    const comp = require('element-ui/lib/' + str2kebab(compName))
+    comp && comp.default && Vue.use(comp.default)
+  })
 }

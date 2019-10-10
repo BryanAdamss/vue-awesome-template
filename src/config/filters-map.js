9509676fc8b9b1e7a4e311f4d9fdc83a9d25ffc3
BryanAdamss <BryanAdamss@foxmail.com>
2019-10-10 09:36:23 +0800

    :wrench: 调整filters-map，添加num-to-chn

--- src/config/filters-map.js
+++ src/config/filters-map.js
@@ -11,10 +11,16 @@ import {
   fixedDecimal
 } from 'Utils'
 
+import NumberToChinese from 'Plugins/num-to-chn'
+
+const num2chn = new NumberToChinese()
+const numberToChn = val => num2chn.transform(val)
+
 export default {
   ms2hms,
   str2kebab,
   dateFormat,
+  numberToChn,
   fixedDecimal,
   num2percentage
 }

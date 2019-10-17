215f8731b0f928dcd688ed64ec687809b23c9a9b
BryanAdamss <BryanAdamss@foxmail.com>
2019-10-17 15:35:34 +0800

    fix(elcompregister): 删除elCompRegister函数及element-ui-list

--- src/plugins/register.js
+++ src/plugins/register.js
@@ -9,7 +9,7 @@ import Vue from 'vue'
 import directivesLoader from 'Plugins/directives-loader'
 
 import filtersMap from 'Config/filters-map'
-import elementUIList from 'Config/element-ui-list'
+// import elementUIList from 'Config/element-ui-list'
 
 export function directiveRegister() {
   const directives = directivesLoader()
@@ -20,10 +20,10 @@ export function filterRegister() {
   Object.entries(filtersMap).forEach(([key, value]) => Vue.filter(key, value))
 }
 
-export function elCompRegister() {
-  const str2kebab = str => str.replace(/\B([A-Z])/g, '-$1').toLowerCase()
-  elementUIList.forEach(compName => {
-    const comp = require('element-ui/lib/' + str2kebab(compName))
-    comp && comp.default && Vue.use(comp.default)
-  })
-}
+// export function elCompRegister() {
+//   const str2kebab = str => str.replace(/\B([A-Z])/g, '-$1').toLowerCase()
+//   elementUIList.forEach(compName => {
+//     const comp = require('element-ui/lib/' + str2kebab(compName))
+//     comp && comp.default && Vue.use(comp.default)
+//   })
+// }

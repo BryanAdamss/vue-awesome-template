f5d6ea714b5dbac911b93929757d6a7d3d19cba9
bryanadamss <bryanadamss@foxmail.com>
2020-04-20 17:38:11 +0800

    feat(all): 添加动态游标基础组件及测试组件

---
+++ src/base/BaseDynamicCursor.vue
@@ -0,0 +1,158 @@
+<template>
+  <component
+    :is="tag"
+    class="c-BaseDynamicCursor">
+
+    <template v-for="(item,index) in list">
+
+      <component
+        :is="itemTag"
+        :key=" item.id || index "
+        :class="[`is-${pos}`]"
+        class="c-BaseDynamicCursor-item">
+
+        <slot v-bind="{item,index}">
+          {{ item.text || item }}
+        </slot>
+
+        <span
+          :style="cursorStyleObj"
+          class="c-BaseDynamicCursor-cursor"/>
+
+      </component>
+
+    </template>
+
+  </component>
+</template>
+
+<script>
+/**
+ * * BaseDynamicCursor
+ * * 基础动态游标
+ */
+
+export default {
+  name: 'BaseDynamicCursor',
+  components: {},
+  mixins: [],
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    },
+    itemTag: {
+      type: String,
+      default: 'span'
+    },
+    list: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    size: {
+      type: [String, Number],
+      default: 2
+    },
+    color: {
+      type: String,
+      default: 'blue'
+    },
+    type: {
+      type: String,
+      default: 'solid'
+    },
+    pos: {
+      type: String,
+      default: 'bottom',
+      validator(val) {
+        return ['top', 'right', 'bottom', 'left'].indexOf(val) > -1
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    cursorStyleObj() {
+      const w = typeof this.size === 'number' ? `${this.size}px` : this.size
+      return {
+        [`border-${this.pos}`]: `${w} ${this.type} ${this.color}`
+      }
+    }
+  },
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.c-BaseDynamicCursor {
+  &-item {
+    position: relative;
+
+    &.is-top,
+    &.is-bottom {
+      .c-BaseDynamicCursor-cursor {
+        top: 0;
+        left: 100%;
+
+        width: 0;
+        height: 100%;
+      }
+
+      &:hover .c-BaseDynamicCursor-cursor {
+        left: 0;
+
+        width: 100%;
+
+        transition-delay: 0.1s;
+      }
+
+      &:hover ~ & {
+        .c-BaseDynamicCursor-cursor {
+          left: 0;
+        }
+      }
+    }
+
+    &.is-left,
+    &.is-right {
+      .c-BaseDynamicCursor-cursor {
+        top: 100%;
+        left: 0;
+
+        width: 100%;
+        height: 0;
+      }
+
+      &:hover .c-BaseDynamicCursor-cursor {
+        top: 0;
+
+        height: 100%;
+
+        transition-delay: 0.1s;
+      }
+
+      &:hover ~ & {
+        .c-BaseDynamicCursor-cursor {
+          top: 0;
+        }
+      }
+    }
+
+    .c-BaseDynamicCursor-cursor {
+      position: absolute;
+      z-index: -1;
+
+      transition: 0.3s all linear;
+
+      content: '';
+    }
+  }
+}
+</style>

cb2da8b2ecb9b92d3d0a4a159b360d08eb09d0f9
bryanadamss <bryanadamss@foxmail.com>
2020-06-18 20:23:18 +0800

    feat(theme): 添加切换主题路由及测试组件

---
+++ src/views/ThemeChangeTest/ThemeChangeTest.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="c-ThemeChangeTest">
+
+    <p class="c-Text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem asperiores, vel officia accusantium sed, nobis non quibusdam quam eos iusto eveniet provident, culpa incidunt ab reprehenderit veritatis! Animi, doloremque sapiente?</p>
+
+    <button @click="handleBtnClick">切换主题</button>
+  </div>
+</template>
+
+<script>
+/**
+ * * ThemeChangeTest
+ */
+
+export default {
+  name: 'ThemeChangeTest',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  mounted() {},
+  methods: {
+    handleBtnClick() {
+      const themeMap = new Map([
+        ['text-color-primary', 'red']
+      ])
+
+      const getCSSText = ([prop, value]) => `--${prop}:${value};`
+
+      const cssText = `:root{${Array.from(themeMap.entries()).map(getCSSText).join()}}`
+
+      const styEl = document.createElement('style')
+      styEl.id = 'themeStyleEl'
+
+      styEl.innerText = cssText
+
+      document.head.appendChild(styEl)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.c-ThemeChangeTest {
+}
+
+.c-Text {
+  color: $mainTextColor;
+}
+</style>

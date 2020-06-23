3c480582596cb86991d38b46fff72a9d4b89f065
bryanadamss <bryanadamss@foxmail.com>
2020-06-23 21:20:08 +0800

    feat(eslint): 补充eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,48 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  // 2020-0623-改用更严格的vue eslit检查规则
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/prettier'],
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    // 在类成员之间出现空行
+    'lines-between-class-members': 'warn',
+    // 强制驼峰
+    camelcase: 'off',
+    // 关闭attribute强制横线连接
+    'vue/attribute-hyphenation': 'off',
+    // 单行内容的html标签，将内容置于一行中
+    'vue/singleline-html-element-content-newline': 'warn',
+    // style、template、script块间留有空格
+    'vue/padding-line-between-blocks': 'warn',
+    // 设置缩进
+    indent: ['error', 2, { SwitchCase: 1 }],
+    // 关闭reject必须返回一个error
+    'prefer-promise-reject-errors': 'off',
+    // 允许同一个var声明多个变量
+    'one-var': 'off',
+    // 允许使用tab
+    'no-tabs': 'off',
+    // 允许函数前没有空格
+    'space-before-function-paren': 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // console、debugger
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
       }
     }
   ]
-};
+}

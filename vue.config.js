8146329a7d6b0d17694a64016f1cde466c61c33b
bryanadamss <bryanadamss@foxmail.com>
2020-06-28 19:16:54 +0800

    feat(svg): 添加svg-sprite支持

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
 const path = require('path')
 
+// 设置svg sprite
+const svgSprite = config => {
+  const svgRule = config.module.rule('svg')
+
+  svgRule.uses.clear()
+
+  svgRule.include
+    .add(path.resolve(__dirname, 'src/assets/svgs'))
+    .end()
+    .use('svg-sprite-loader')
+    .loader('svg-sprite-loader')
+    .options({})
+    .end()
+    .use('svgo-loader')
+    .loader('svgo-loader')
+    .options({
+      plugins: [{ removeViewBox: false }, { removeXMLNS: true }]
+    })
+    .end()
+}
+
 module.exports = {
+  chainWebpack: config => {
+    svgSprite(config)
+  },
   pluginOptions: {
     // vue-cli-plugin-auto-alias 配置
     'vue-cli-plugin-auto-alias': {

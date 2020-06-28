6d33993372a990ea28da79300d10a7ba78c4f1ac
bryanadamss <bryanadamss@foxmail.com>
2020-06-28 11:01:48 +0800

    feat(scss): 添加vue-cli-plugin-style-resources-loader

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
+const path = require('path')
+
 module.exports = {
   pluginOptions: {
     'vue-cli-plugin-auto-alias': {
       rootDirName: 'src',
       alias: {}
+    },
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/sass/utils/_variablesCustom-m.scss'),
+        path.resolve(__dirname, 'src/sass/utils/_variables.scss'),
+        path.resolve(__dirname, 'src/sass/utils/_function.scss'),
+        path.resolve(__dirname, 'src/sass/utils/_mixins.scss'),
+        path.resolve(__dirname, 'src/sass/utils/_mediaQuery.scss'),
+        path.resolve(__dirname, 'src/sass/utils/_placeholders.scss')
+      ]
     }
   }
 }

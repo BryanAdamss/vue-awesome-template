bcc45ba9491074f7a8c5784d748c9972ea0ed1c6
bryanadamss <bryanadamss@foxmail.com>
2020-08-01 13:50:23 +0800

    refactor(loading): 更新global-loading

--- src/plugins/global-loading.js
+++ src/plugins/global-loading.js
@@ -16,7 +16,7 @@ class GlobalLoading {
     showEventName = 'global.loading.show', // loading show时派发的事件名
     hideEventName = 'global.loading.hide', // loading hide时派发的事件名
     minReqTime = 400, // loading展示所需要的最少请求时间，请求时间超过400ms才会展示loading
-    maxCombinTime = 1000, // 两次独立loading合并的最大间隔时间
+    maxCombinTime = 200, // 两次独立loading合并的最大间隔时间
     debug = false // 调试模式
   } = {}) {
     this.showEventName = showEventName
@@ -31,21 +31,23 @@ class GlobalLoading {
     this.startTime = null
     this.endTime = null
 
-    this.count = 0
+    this.count = 0 // 请求计数
   }
 
   start() {
-    this.startTime = this._getNow()
+    // 记录首个请求发送时间
+    if (!this.startTime) this.startTime = this._getNow()
 
-    if (this.hideTimer) {
-      clearTimeout(this.hideTimer)
-      this.hideTimer = null
-    }
+    // loading关闭的maxCombinTime时间内又有新请求发送
+    // 则清除hideTimer，以此来合并loading展示
+    if (this.hideTimer) this._cleanHideTimer()
 
     this.count++
 
     this.debug && console.log('start count', this.count)
 
+    // 延迟minReqTime时间展示loading
+    // 以此来屏蔽响应时间小于minReqTime的请求loading展示
     if (!this.showTimer) {
       this.showTimer = setTimeout(() => {
         this.debug && console.log('show')
@@ -55,6 +57,7 @@ class GlobalLoading {
   }
 
   end() {
+    // 记录每个请求的结束时间点
     this.endTime = this._getNow()
 
     this.count--
@@ -67,33 +70,32 @@ class GlobalLoading {
 
       this.debug && console.log('持续时间', this.endTime - this.startTime)
 
-      // 时间<400ms取消loading的展示
+      // 时间小于minReqTime，取消showTimer中的loading展示
       if (this.endTime - this.startTime < this.minReqTime) {
         this.debug && console.log(`< ${this.minReqTime}ms，取消loading的展示`)
 
-        clearTimeout(this.showTimer)
-        this.showTimer = null
+        this._cleanShowTimer()
       } else {
-        // >400ms，则展示loading，并延迟1s关闭弹窗
-        // 1s内，若有新的请求到达，则合并展示loading
+        // 大于minReqTime，则showTimer中的loading会展示
+        // 为了合并两个独立请求，延迟maxCombinTime关闭弹窗
+        // maxCombinTime内，若有新的请求到达，则合并展示loading
         this.debug &&
           console.log(
             `> ${this.minReqTime}ms，延迟${this.maxCombinTime}ms关闭弹窗`
           )
 
-        this.hideTimer = setTimeout(() => {
-          this.debug && console.log('close')
+        if (!this.hideTimer) {
+          this.hideTimer = setTimeout(() => {
+            this.debug && console.log('close')
 
-          this._hide()
-          this._clean()
-        }, this.maxCombinTime)
+            this.hide()
+          }, this.maxCombinTime)
+        }
       }
     }
   }
 
   hide() {
-    this.count = Math.max(--this.count, 0)
-
     this._hide()
     this._clean()
   }
@@ -111,11 +113,23 @@ class GlobalLoading {
   }
 
   _clean() {
-    clearTimeout(this.showTimer)
-    clearTimeout(this.hideTimer)
+    this._cleanShowTimer()
+    this._cleanHideTimer()
 
+    this._cleanStartAndEnd()
+  }
+
+  _cleanShowTimer() {
+    clearTimeout(this.showTimer)
     this.showTimer = null
+  }
+
+  _cleanHideTimer() {
+    clearTimeout(this.hideTimer)
     this.hideTimer = null
+  }
+
+  _cleanStartAndEnd() {
     this.startTime = null
     this.endTime = null
   }

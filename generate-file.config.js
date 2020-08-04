c7098014bee423dc913c611607bcfd8fc153f3b2
bryanadamss <bryanadamss@foxmail.com>
2020-08-04 17:40:20 +0800

    feat(pkg): 添加new:comp命令

---
+++ generate-file.config.js
@@ -0,0 +1,10 @@
+/**
+ * @author GuangHui
+ * @description @bryanadamss/generate-file 配置文件
+ */
+
+module.exports = {
+  case: 'pascalCase',
+  output: 'src/views',
+  template: '.vue-comp-template'
+}

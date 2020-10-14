306622b76ecaa9426457e12dbcf61f6317229966
bryanadamss <bryanadamss@foxmail.com>
2020-10-14 16:30:01 +0800

    chore(pdf): 优化提示

--- src/base/BasePdfViewer/ErrorTips.vue
+++ src/base/BasePdfViewer/ErrorTips.vue
@@ -7,13 +7,17 @@ export default {
   name: 'ErrorTips',
   functional: true,
   render(h, { data, children }) {
+    const tips = data.attrs && data.attrs.tips
+      ? data.attrs.tips
+      : '加载出错，请尝试其它方式预览'
+
     return h('p', {
       style: {
         color: '#fff',
         'font-size': '16px',
         'text-align': 'center'
       }
-    }, children || '加载出错，请尝试其它方式预览')
+    }, children || tips)
   }
 }
 </script>

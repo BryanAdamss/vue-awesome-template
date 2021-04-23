70d88c1ce1ec8cfba2b12ae3d846524fc9c5a4b7
bryanadamss <bryanadamss@foxmail.com>
2021-04-23 16:08:25 +0800

    chore(commitlint): 更新commitlint配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,35 @@
  */
 
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'always',
+      [
+        'lower-case', // default
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'kebab-case', // kebab-case
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'snake-case', // snake_case
+        'start-case' // Start Case
+      ]
+    ],
+    'scope-case': [
+      2,
+      'always',
+      [
+        'lower-case', // default
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'kebab-case', // kebab-case
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'snake-case', // snake_case
+        'start-case' // Start Case
+      ]
+    ]
+  }
 }

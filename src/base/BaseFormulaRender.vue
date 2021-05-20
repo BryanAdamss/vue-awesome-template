c90564186e896526b7729ab07804fbb43ba2ad8b
bryanadamss <bryanadamss@foxmail.com>
2021-05-20 15:55:15 +0800

    chore(comp): 优化渲染逻辑

--- src/base/BaseFormulaRender.vue
+++ src/base/BaseFormulaRender.vue
@@ -1,8 +1,4 @@
 <script>
-/**
- * @description 基础公式渲染组件
- */
-
 import { uuid } from 'Utils'
 
 import KatexWrapper from 'Services/extends/katex-wrapper'
@@ -11,6 +7,9 @@ import MathJaxWrapper from 'Services/extends/mathjax-wrapper'
 const katex = new KatexWrapper()
 const mathjax = new MathJaxWrapper()
 
+/**
+ * @description 基础公式渲染组件
+ */
 export default {
   name: 'BaseFormulaRender',
   props: {
@@ -21,27 +20,36 @@ export default {
   },
   data() {
     return {
-      html: ''
+      html: '', 
+      id: uuid()
     }
   },
-  created() {
-    this.id = uuid()
+  watch: {
+    htmlString: {
+      handler: 'handleHtmlStringChange',
+      immediate: true
+    }
+  },
+  methods: {
+    handleHtmlStringChange(val, oldVal) {
+      if (val === oldVal) return
 
-    katex
-      .render(this.htmlString)
-      .then(katexedStr => {
-        this.html = katexedStr
-      })
-      .catch(err => {
-        console.log('katex render error', err)
-
-        this.html = this.htmlString
-
-        // 确保节点内有内容，再渲染
-        this.$nextTick(() => {
-          mathjax.render(this.id)
+      katex.render(this.htmlString)
+        .then(katexedStr => {
+          console.log('katexedStr', katexedStr)
+          this.html = katexedStr
         })
-      })
+        .catch(err => {
+          console.log('katex render error', err)
+
+          this.html = this.htmlString
+
+          // 确保节点内有内容，再渲染
+          this.$nextTick(() => {
+            mathjax.render(this.id)
+          })
+        })
+    }
   },
   render(h) {
     return h('div', {

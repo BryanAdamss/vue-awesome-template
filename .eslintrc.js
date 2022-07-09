8b3e2da9b67ec223c847667ff3cedacd276974ef
BryanAdamss <bryanadamss@foxmail.com>
2022-07-09 22:46:04 +0800

    chore(eslint): make eslint lint .vite-config dir

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/**
+ * @author GuangHui
+ * @description eslint配置，继承@antfu/eslint-config
+ */
+
+module.exports = {
+  root: true,
+  extends: '@antfu',
+  rules: {
+    'no-console': 'off',
+  },
+}

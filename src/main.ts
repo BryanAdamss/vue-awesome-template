8becb4bf4b57a7efd410e1a8478320d6d2c1bd68
BryanAdamss <bryanadamss@foxmail.com>
2022-07-15 17:08:42 +0800

    chore: add router-instance

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from '@/services/instance/router-instance'
 
 const app = createApp(App)
 
--- src/services/instance/router-instance.ts
+++ src/services/instance/router-instance.ts
@@ -1,5 +1,10 @@
+/**
+ * @author GuangHui
+ * @description 路由实例
+ */
+
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +20,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/AboutView.vue'),
     },
   ],
 })

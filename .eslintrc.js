efb6e8f6d8e3991c1b5d2c918bc2f0c42e0e3e79
BryanAdamss <bryanadamss@foxmail.com>
2022-08-09 16:45:30 +0800

    feat: 自定义import排序

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,37 @@
 module.exports = {
   root: true,
   extends: '@antfu',
+  plugins: ['import'],
   rules: {
     'no-console': 'off',
+    /* eslint-plugin-import */
+    'import/order': [
+      'warn',
+      {
+        'groups': [
+          'type', /* ts、flow专有，import type {} from 'xxx' */
+          'builtin', /* node内置 */
+          'external', /* 外部npm模块 */
+          'internal', /* 内部模块，一般为webpack alias目录模块 */
+          'parent', /* 父级目录 ../ */
+          'sibling', /* 相邻陌路 ./ */
+          'index', /* index导入 */
+          'object', /* ts专有，import log = console.log */
+        ],
+        'pathGroups': [
+          /* @/归为internal组 */
+          {
+            pattern: '@/**',
+            group: 'internal',
+          },
+        ],
+        'newlines-between': 'always', /* 组间强制空行 */
+        /* 组内字母排序规则 */
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 }
--- README.md
+++ README.md
@@ -43,5 +43,5 @@ If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has a
 - [ ] 组件模板生成/移除
 - [ ] `i18n`支持
 - [x] 支持`event-bus`（基于`EventEmitter3`）
-- [ ] 自定义`import语句`顺序
+- [x] 自定义`import语句`顺序
 - [x] 支持`CI构建`，可通过`pnpm build:prod -- --ciVersion 1.1.1`添加构建版本号

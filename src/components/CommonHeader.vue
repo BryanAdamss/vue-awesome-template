aff59d1952f0af4a4b4c57ecc87276f3a5c2269b
BryanAdamss <bryanadamss@foxmail.com>
2022-08-22 17:16:26 +0800

    chore: prettify header

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -5,8 +5,19 @@
 </script>
 
 <template>
-  <header class="c-CommonHeader">
-    <span class="hover:cursor-pointer hover:text-red-700" @click="$router.back()">&lt;</span>
-    <span>vue-awesome-template</span>
+  <header class="c-CommonHeader flex items-center ">
+    <div class="flex-none w-8 p-1" @click="$router.back()">
+      <div v-show="$route.path !== '/'" class="text-center hover:cursor-pointer hover:bg-gray-300">
+        &lt;
+      </div>
+    </div>
+    <div class="flex-auto p-1">
+      <h1 class="text-center font-bold">
+        VUE-AWESOME-TEMPLATE
+      </h1>
+    </div>
+    <div class="flex-none p-1">
+      <span>right</span>
+    </div>
   </header>
 </template>
